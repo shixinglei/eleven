e8fead03b294871a42b4826fcaf99d581bdcdaf8
shixinglei <875118797@qq.com>
2020-06-25 06:50:19 +0800

    hello-springcloud-alibaba

---
+++ spring-cloud-alibaba-dependencies/src/main/java/com/example/springcloud/alibaba/dependencies/SpringCloudAlibabaDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.alibaba.dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaDependenciesApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/example/springcloud/alibaba/nacos/consumer/feign/controller/NacosConsumerFeignController.java
@@ -0,0 +1,17 @@
+package com.example.springcloud.alibaba.nacos.consumer.feign.controller;
+
+import com.example.springcloud.alibaba.nacos.consumer.feign.service.EchoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NacosConsumerFeignController {
+    @Autowired
+    private EchoService echoService;
+
+    @GetMapping(value = "/echo/hi")
+    public String echo() {
+        return echoService.echo("Hi Feign");
+    }
+}
